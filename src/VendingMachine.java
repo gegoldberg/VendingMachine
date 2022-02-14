4dba7fdc79af0e62cfc3b2f3638bda3e58fedaa1
Garrett <ggoldbergtwin@gmail.com>
2022-02-14 16:46:16 -0500

    Initial Commit

---
+++ src/VendingMachine.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class VendingMachine {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to the Vending Machine. You will be asked to type a number that is coordinated to the item you would like.");
+		
+	}
+
+}
